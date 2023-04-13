7693f0a69a01694c3975b96066a3f9620333338f
lbandeira <lbms@cin.ufpe.br>
2023-04-13 09:42:20 -0300

    Adiciona classe de manipulacao de lista

---
+++ PLP-linguagens/Funcional2/src/lf2/plp/functional2/declaration/ManipLista.java
@@ -0,0 +1,32 @@
+package lf2.plp.functional2.declaration;
+
+public class ManipLista {
+
+    public boolean estaEmSequencia() {
+        if (lista.size() <= 1) {
+            return true;
+        }
+          
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i) != i + lista.get(0)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    public boolean temParidade() {
+        for (int i = 1; i < lista.size(); i++) {
+            if (lista.get(i).equals(lista.get(i - 1))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean contadorDeSimilares() {
+        
+        return false;
+    }
+}
